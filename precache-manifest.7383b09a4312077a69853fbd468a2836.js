ac30be278f03b5a0aea16dad70c6874f9b0f1dad
Jared Giangrasso <jagiangrasso@gmail.com>
2019-02-13 18:32:38 -0500

    Updates

---
+++ precache-manifest.7383b09a4312077a69853fbd468a2836.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "def32014c07cb399eb7cffe9ca3c5052",
+    "url": "/static/media/weather-screen-small.def32014.png"
+  },
+  {
+    "revision": "e294254c4824b7cc6d7f0b5be6ec6e21",
+    "url": "/static/media/practice-screen-small.e294254c.png"
+  },
+  {
+    "revision": "52c8ae9d1d9daea72e3fa1b5d8ee8251",
+    "url": "/static/media/penguin-screen-small.52c8ae9d.png"
+  },
+  {
+    "revision": "c2c55354ebbe06e5ad61d3a5d3d88bdf",
+    "url": "/static/media/TEMP_Headshot.c2c55354.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "462270817b4797eff657",
+    "url": "/static/js/main.46227081.chunk.js"
+  },
+  {
+    "revision": "61edec513a048369d829",
+    "url": "/static/js/1.61edec51.chunk.js"
+  },
+  {
+    "revision": "462270817b4797eff657",
+    "url": "/static/css/main.9df78944.chunk.css"
+  },
+  {
+    "revision": "f24fbfbba3ed3b24c0077e4724545414",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d7e3c6d444c282491c9c743c0a76c61.js"
+  "/precache-manifest.7383b09a4312077a69853fbd468a2836.js"
 );
 
 workbox.clientsClaim();
