5ed5681352794d21ec8a1bfb7ebf1ba6c897eb77
Jared Giangrasso <jagiangrasso@gmail.com>
2019-02-15 14:30:39 -0500

    Updates

--- precache-manifest.9da1fe82399f09821551cca79a1450cc.js
+++ precache-manifest.9da1fe82399f09821551cca79a1450cc.js
@@ -12,31 +12,31 @@ self.__precacheManifest = [
     "url": "/static/media/penguin-screen-small.52c8ae9d.png"
   },
   {
-    "revision": "c2c55354ebbe06e5ad61d3a5d3d88bdf",
-    "url": "/static/media/TEMP_Headshot.c2c55354.jpg"
-  },
-  {
     "revision": "d952f26f61be01c13b17c08972f1d7cc",
     "url": "/static/media/Jared_Giangrasso_Resume.d952f26f.pdf"
+  },
+  {
+    "revision": "12197298d50949bbac6966cf82038cfa",
+    "url": "/static/media/Headshot.12197298.jpeg"
   },
   {
     "revision": "229c360febb4351a89df",
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "64c76c0289a87069bba3",
-    "url": "/static/js/main.64c76c02.chunk.js"
+    "revision": "700cdd043887eedb2447",
+    "url": "/static/js/main.700cdd04.chunk.js"
   },
   {
     "revision": "35d5f88e1868fa0c9346",
     "url": "/static/js/1.35d5f88e.chunk.js"
   },
   {
-    "revision": "64c76c0289a87069bba3",
+    "revision": "700cdd043887eedb2447",
     "url": "/static/css/main.428816de.chunk.css"
   },
   {
-    "revision": "8cb725d776f6749caba84ef49b389da4",
+    "revision": "9548ee9304f9438aaff5ba02e7a12e07",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.afcc6e44c31355a358203bd03c73bfe5.js"
+  "/precache-manifest.9da1fe82399f09821551cca79a1450cc.js"
 );
 
 workbox.clientsClaim();
